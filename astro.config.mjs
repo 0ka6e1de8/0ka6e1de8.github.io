cc9c446e2ed177ce9d00cea05d86938bc7a61822
0ka6e1de8 <0ka6e1de8@gmail.com>
2025-05-22 23:03:46 +0900

    途中

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
 // @ts-check
-import {defineConfig} from "astro/config";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  base: process.env.PUBLIC_BASE || "/",
+  base: process.env.PUBLIC_BASE || '/',
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use '@/styles/foundation' as *;`,
+        },
+      },
+    },
+  },
 });
